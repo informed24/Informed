5c73e763696cff8126b25e2d87df12fc68b6b20b
David Soares Vieira <a56550@alunos.ipb.pt>
2024-11-22 16:13:57 +0000

    Feat Change logic from theme switch

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,14 +1,20 @@
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.body;
 
-themeSwitch.addEventListener('click', () => {
-  body.classList.toggle('dark-theme');
-
-  const isDarkTheme = body.classList.contains('dark-theme');
-  const circle = document.querySelector('.toggle-circle');
-  if (!isDarkTheme) {
-    circle.style.transform = 'translateX(44px)';
-  } else {
-    circle.style.transform = 'translateX(0px)';
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme') || 'light-theme';
+  body.classList.add(savedTheme);
+  updateToggleCircle(savedTheme === 'dark-theme');
 });
+
+themeSwitch.addEventListener('click', () => {
+  const isDarkTheme = body.classList.toggle('dark-theme');
+  const currentTheme = isDarkTheme ? 'dark-theme' : 'light-theme';
+  localStorage.setItem('theme', currentTheme);
+  updateToggleCircle(!isDarkTheme);
+});
+
+function updateToggleCircle(isDarkTheme) {
+  const circle = document.querySelector('.toggle-circle');
+  circle.style.transform = isDarkTheme ? 'translateX(44px)' : 'translateX(0px)';
+}
